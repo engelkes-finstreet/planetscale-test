be828081b92fc098592b62c6fea9f3c4b74adc46
Patrick Engelkes <patrick.engelkes@finstreet.de>
2022-05-09 14:05:06 +0200

    More Vercel config.

--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,9 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverBuildTarget: "vercel",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
 };
